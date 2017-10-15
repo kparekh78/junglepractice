1ac7376ac21ba3080c8036feda901df3ec50203a
Mansi Parekh <pmana79@gmail.com>
2017-10-15 15:50:29 -0700

    initial commit

---
+++ src/main/java/com/wellsfargo/practice/jungletest/App.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.practice.jungletest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/wellsfargo/practice/jungletest/SockTestIT.java
@@ -0,0 +1,30 @@
+package com.wellsfargo.practice.jungletest;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SockTestIT {
+
+	static WebDriver driver;
+	
+	/*@BeforeClass
+	public static void launchBrowser(){
+		driver = new ChromeDriver();
+		
+		driver.navigate().to("https://jungle-socks.herokuapp.com/");
+		
+	}
+	*/
+	
+	@Test
+	public void elementFind(){
+		
+		assertEquals("mansi", "mansi");
+	}
+
+	
+}
